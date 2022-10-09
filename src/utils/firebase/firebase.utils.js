45e3fa504dbb145a60fec10879989a8cc64c98af
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-10-10 01:35:20 +0530

    Added google sinin and firebase

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAWQGwaAqczd70DEsbilE5eS0CR9GnUHc",
+  authDomain: "cloth-shopping-portal.firebaseapp.com",
+  projectId: "cloth-shopping-portal",
+  storageBucket: "cloth-shopping-portal.appspot.com",
+  messagingSenderId: "80544277177",
+  appId: "1:80544277177:web:497ef6169a2cf15d39b168",
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
