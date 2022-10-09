8558b82ce0f8cd9263718205b86e7148fc234932
rockxxxxxx <78143717+rockxxxxxx@users.noreply.github.com>
2022-10-10 00:08:28 +0530

    Imported the svg logo

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,10 +1,11 @@
 import {Outlet,Link} from 'react-router-dom';
+import {ReactComponent as CompanyLogo} from '../../assests/crown.svg';
 const Navigation=()=>{
     return(
         <>
         <div className='navigation'>
             <Link className='logo-container' to="/">
-                Logo
+                <CompanyLogo className='logo'/>
             </Link>
             <Link className='navigation-links-container'>
                 <Link className='nav-link' to="/shop">
